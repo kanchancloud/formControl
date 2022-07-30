b7228a056ad204b2f91782c1facc7ddbdef62b88
kanchan <kanchansharma1408@gmail.com>
2022-07-30 17:41:15 +0530

    first commit

---
+++ src/Component/header/LoginPageBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "antd";
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Button type="submit" className="Login_button">Login</Button>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ src/Component/login/FormBtnPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormButtonPage = () => {
+    return (
+        <div className="Login_Button_Wrap">
+            <button className="Login_Button">Login</button>
+        </div>
+    );
+};
+
+export default FormButtonPage;
